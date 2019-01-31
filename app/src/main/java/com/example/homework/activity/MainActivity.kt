3bd1a3e909ef7d60852ad9f96dae971419bb186f
ShrayChona <shraychona@gmail.com>
2019-01-31 16:33:53 +0530

    pesdk integration start

--- app/src/main/java/com/example/homework/activity/MainActivity.kt
+++ app/src/main/java/com/example/homework/activity/MainActivity.kt
@@ -16,14 +16,8 @@
 
 package com.example.homework.activity
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.homework.R
 
 class MainActivity : AppCompatActivity() {
@@ -32,52 +26,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.button_gallery).setOnClickListener {
-            startActivity(Intent(this, GalleryActivity::class.java))
-        }
-        findViewById<View>(R.id.button_camera).setOnClickListener {
-            if (!hasCameraPermission() || !hasStoragePermission()) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    REQUEST_CAMERA
-                )
-            } else {
-                startActivity(Intent(this, CameraActivity::class.java))
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == REQUEST_CAMERA && grantResults.size == 2
-            && grantResults[0] == PackageManager.PERMISSION_GRANTED
-            && grantResults[1] == PackageManager.PERMISSION_GRANTED
-        ) {
-            startActivity(Intent(this, CameraActivity::class.java))
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-    }
-
-    private fun hasCameraPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun hasStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    companion object {
-        private const val REQUEST_CAMERA = 1
     }
 }
